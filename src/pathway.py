70f76e535b5f401af5e7e35435019f584b12cbdf
Spicyz <bossthth1209mine@gmail.com>
2020-12-10 12:39:50 +0700

    update pathway

---
+++ src/pathway.py
@@ -0,0 +1,18 @@
+subject = [
+    "1. MATH",
+    "2. PHYSICS",
+    "0. TERMINATE"
+]
+subjects = {
+    1:
+    [
+        "1. Metric",
+        "2. Sets",
+        "3. Surface area and Volume",
+    ],
+
+    2:
+    [
+
+    ]
+}
