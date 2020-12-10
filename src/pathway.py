03cff4feeb6445e93e6a71692a474daf8dcdfd86
Nattapat Buakhamkote <63070057@it.kmitl.ac.th>
2020-12-10 14:25:19 +0700

    :computer: Sort and add dict

--- src/pathway.py
+++ src/pathway.py
@@ -1,18 +1,33 @@
-subject = [
+subject_list = [
     "1. MATH",
     "2. PHYSICS",
     "0. TERMINATE"
 ]
 subjects = {
+    # Math
     1:
-    [
-        "1. Metric",
-        "2. Sets",
-        "3. Surface area and Volume",
-    ],
+    {
+    # Add more sub-subject here
+        "1. Metric":
+        {
+        # Insert function here
+            ''
+        },
+        "2. Sets":
+        {
+        # Insert function here
+            ''
+        },
+        "3. Surface area and Volume":
+        {
+        # Insert function here
+            ''
+        }
+    },
 
+    # Physics
     2:
-    [
+    {
 
-    ]
+    }
 }
