0a1d8839b7ca0853c2695eed0df465589327563d
Nattapat Buakhamkote <63070057@it.kmitl.ac.th>
2020-12-12 14:38:04 +0700

    :warning: Not finished

--- src/pathway.py
+++ src/pathway.py
@@ -8,10 +8,14 @@ content = {
     1:
     {
     # Add more sub-subject here
-        "1. Metric":
+        "1. Metrix":
         {
         # Insert function here
-            ''
+            '1. Addition': 'add_sub(add)',
+            '2. Subtraction': 'add_sub(sub)',
+            '3. Multiplication': 'multi()',
+            '4. Determinant': 'find_det()',
+            '5. Cramer\'s rule': 'cramer()'
         },
         "2. Sets":
         {
@@ -22,6 +26,12 @@ content = {
         {
         # Insert function here
             ''
+        },
+        "4. Staticstic":
+        {
+        # Insert function here
+            '1.': 'statistics1()',
+            '2.': 'statistics2()'
         },
         "0. Back":
         {
