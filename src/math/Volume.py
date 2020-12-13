1c0035c97ddfed09e205f48f1adbca4544367ce3
it63070045 <73681882+it63070045@users.noreply.github.com>
2020-12-14 02:55:07 +0700

    :bell: Delete "/n"

--- src/math/Volume.py
+++ src/math/Volume.py
@@ -1,20 +1,20 @@
 import math
-def Cylinder():###ทรงกระบอก
+def Cylinder():#ทรงกระบอก
     radius = int(input("ใส่ค่ารัศมี(cm)"))
     space = math.pi * radius**2
     high = int(input("ใส่ค่าความสูง(cm)"))
     print("%.3f" %(high * space))    
 Cylinder()
-def cube():
+def cube():#เหลียม
     side = int(input("ใส่ค่าของด้าน(cm)"))
     high = int(input("ใส่ค่าความสูง(cm)"))
     print(side**2 * high, "เหลี่ยม")
 cube()
-def circle():
+def circle():#วงกลม
     radius = int(input("ใส่ค่ารัศมี(cm)"))
     print(4 * math.pi * radius**3 / 3)
 circle()
-def pyramid():
+def pyramid():#ปิรามิด
     typ = input("type(triangle, cube")
     lon = int(input("ใส่ค่าของด้าน(cm)"))
     highl = int(input("ใส่ค่าของด้าน(cm)"))
@@ -25,12 +25,12 @@ def pyramid():
     elif typ == "cube":
         print(volume * high/3)
 pyramid()
-def Conical():
+def Conical():#ทรงกรวย
     radius = int(input("ใส่ค่ารัศมี(cm)"))
     high = int(input("ใส่ค่าความสูง(cm)"))
     print(math.pi * radius**2 * high / 3)
 Conical()
-def prism():
+def prism():#ปริซึม
     typ = input("type(triangle, cube")
     if typ == "triangle" or typ == "cube":
         lon = int(input("ใส่ค่าของด้าน(cm)"))
