b92a6467caa50174f5d3394f8db6cfd31f007284
it63070045 <73681882+it63070045@users.noreply.github.com>
2020-12-14 03:44:28 +0700

    :bell: Delete "/n"

--- src/math/Volume.py
+++ src/math/Volume.py
@@ -1,49 +1,49 @@
 import math
 def Cylinder():#ทรงกระบอก
-    radius = int(input("ใส่ค่ารัศมี(cm)"))
+    radius = int(input("ใส่ค่ารัศมี(cm) >>> "))
     space = math.pi * radius**2
-    high = int(input("ใส่ค่าความสูง(cm)"))
+    high = int(input("ใส่ค่าความสูง(cm) >>> "))
     print("%.3f" %(high * space))    
 Cylinder()
 def cube():#เหลียม
-    side = int(input("ใส่ค่าของด้าน(cm)"))
-    high = int(input("ใส่ค่าความสูง(cm)"))
-    print(side**2 * high, "เหลี่ยม")
+    side = int(input("ใส่ค่าของด้าน(cm) >>> "))
+    high = int(input("ใส่ค่าความสูง(cm) >>> "))
+    print("%.3f" %(side**2 * high))
 cube()
 def circle():#วงกลม
-    radius = int(input("ใส่ค่ารัศมี(cm)"))
-    print(4 * math.pi * radius**3 / 3)
+    radius = int(input("ใส่ค่ารัศมี(cm) >>> "))
+    print("%.3f" %(4 * math.pi * radius**3 / 3))
 circle()
 def pyramid():#ปิรามิด
-    typ = input("type(triangle, cube")
-    lon = int(input("ใส่ค่าของด้าน(cm)"))
-    highl = int(input("ใส่ค่าของด้าน(cm)"))
-    high = int(input("ใส่ค่าความสูง(cm)"))
+    typ = input("triangle, cube >>> ")
+    lon = int(input("ใส่ค่าของด้าน(cm) >>> "))
+    highl = int(input("ใส่ค่าของด้าน(cm) >>> "))
+    high = int(input("ใส่ค่าความสูง(cm) >>> "))
     volume = lon * highl
     if typ == "triangle":
-        print((volume * 0.5) * high/3)
+        print("%.3f" %((volume * 0.5) * high/3))
     elif typ == "cube":
-        print(volume * high/3)
+        print("%.3f" %(volume * high/3))
 pyramid()
 def Conical():#ทรงกรวย
-    radius = int(input("ใส่ค่ารัศมี(cm)"))
-    high = int(input("ใส่ค่าความสูง(cm)"))
-    print(math.pi * radius**2 * high / 3)
+    radius = int(input("ใส่ค่ารัศมี(cm) >>> "))
+    high = int(input("ใส่ค่าความสูง(cm) >>> "))
+    print("%.3f" %(math.pi * radius**2 * high / 3))
 Conical()
 def prism():#ปริซึม
-    typ = input("type(triangle, cube")
+    typ = input("triangle, cube >>> ")
     if typ == "triangle" or typ == "cube":
-        lon = int(input("ใส่ค่าของด้าน(cm)"))
-        highl = int(input("ใส่ค่าของด้าน(cm)"))
-        high = int(input("ใส่ค่าความสูง(cm)"))
+        lon = int(input("ใส่ค่าของด้าน(cm) >>> "))
+        highl = int(input("ใส่ค่าของด้าน(cm) >>> "))
+        high = int(input("ใส่ค่าความสูง(cm) >>> "))
         volume = lon * highl
         if typ == "triangle":
-            print((volume * 0.5) * high)
+            print("%.3f" %((volume * 0.5) * high))
         elif typ == "cube":
             print(volume * high)
     elif typ == "circle":
-        radius = int(input("ใส่ค่ารัศมี(cm)"))
-        high = int(input("ใส่ค่าความสูง(cm)"))
+        radius = int(input("ใส่ค่ารัศมี(cm) >>> "))
+        high = int(input("ใส่ค่าความสูง(cm) >>> "))
         cir = radius * math.pi
-        print(cir * high)
+        print("%.3f" %(cir * high))
 prism()
