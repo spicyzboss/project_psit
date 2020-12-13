8b0f03b1b0fffe07bc57364300cde5825500fe87
Nattapat Buakhamkote <63070057@it.kmitl.ac.th>
2020-12-13 19:33:05 +0700

    ✅ Finished function

--- src/math/sequence_series.py
+++ src/math/sequence_series.py
@@ -28,7 +28,7 @@ def sequence():
         print()
         ans_arimethic = first_arimethic+((number_arimethic-1)*diff_arimethic)
         print("The %d term of Sequence is %.2f"%(number_arimethic, ans_arimethic))
-        
+
     elif type_sequence == "GEOMETRIC SEQUENCE":
         delay(0.25)
         print("This Topic will find N term of the Geometric Sequence")
@@ -42,7 +42,7 @@ def sequence():
         print()
         ans_geometric = first_geometric*(rat_geometric**(number_geometric-1))
         print("The %d term of Sequence is %.2f"%(number_geometric, ans_geometric))
-        
+
 #_____________________________________END SEQUENCE______________________________________
 def sigma():
     """FIND SUM OF 1toN 1^2toN^2 and 1^3toN^3"""
@@ -65,7 +65,7 @@ def sigma():
         ans_sigma_i = (n_sigma*(n_sigma+1))/2
         print()
         print("The Summation of 1 to %d is %.2f"%(n_sigma, ans_sigma_i))
-        
+
     elif type_sigma == "I^2":
         delay(0.25)
         print("This form will find the sum of 1^2+2^2+3^2+...+N^2 (\"^\" mean Exponential)")
@@ -75,7 +75,7 @@ def sigma():
         ans_sigma_i2 = (n_sigma_2*(n_sigma_2+1)*(2*n_sigma_2+1))/6
         print()
         print("The Summation of 1^2 to %d^2 is %.2f"%(n_sigma_2, ans_sigma_i2))
-        
+
     elif type_sigma == "I^3":
         delay(0.25)
         print("This form will find the sum of 1^3+2^3+3^3+...+N^3 (\"^\" mean Exponential)")
@@ -85,7 +85,7 @@ def sigma():
         ans_sigma_i3 = ((n_sigma_3*(n_sigma_3+1))/2)**2
         print()
         print("The Summation of 1^3 to %d^3 is %.2f"%(n_sigma_3, ans_sigma_i3))
-        
+
 #_____________________________________END SIGMA______________________________________
 def series():
     """Find series of arithmetic and geometric"""
@@ -120,7 +120,7 @@ def series():
             ans_arith_series = (number_arimethic_series/2)*((2*first_arimethic_series)+((number_arimethic_series-1)*diff_arimethic_series))
             print()
             print("The %d term of Series is %.2f"%(number_arimethic_series, ans_arith_series))
-            
+
         elif type_ari_series == "INFINITE":
             delay(0.25)
             print("This topic will find infinite term of Arithmetic Series")
@@ -134,7 +134,7 @@ def series():
                 print("The infinite term of this Series is 0(Convergent Series)")
             else:
                 print("This Series can't find Infinite term(Divergent Series)")
-            
+
     elif type_series == "GEOMETRIC SERIES":
         delay(0.25)
         print("Which Geometric Series do you want to find?")
@@ -161,7 +161,7 @@ def series():
                 ans_geo_series = (first_geometric_series*(1-(rat_geometric_series**number_geometric_series)))/(1-rat_geometric_series)
             print()
             print("The %d term of Series is %.2f"%(number_geometric_series, ans_geo_series))
-            
+
         elif type_geo_series == "INFINITE":
             delay(0.25)
             print("This topic will find infinite term of Geometric Series")
@@ -176,17 +176,13 @@ def series():
                 print("The infinite term of this Series is %.2f(Convergent Series)"%(ans_geo_series))
             else:
                 print("This Series can't find Infinite term(Divergent Series)")
-            
+
 #_____________________________________END SERIES______________________________________
 
 def sequence_sigma_series():
     """Main Function"""
-    print()
-    print("SEQUENCE & SIGMA & SERIES")
-    print()
-    print("► Sequence\t\t► Sigma\t\t► Series")
-    
-    print()
+    print("SEQUENCE & SIGMA & SERIES\n")
+    print("► Sequence\t\t► Sigma\t\t► Series\n")
     print("What topic do you want to find?: ", end="")
     type_input_3s = input().upper()
     if type_input_3s == "SEQUENCE":
