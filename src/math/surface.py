e18c808050cc10c9204772d8c26e3944cbea3fa5
it63070045 <73681882+it63070045@users.noreply.github.com>
2020-12-14 14:52:18 +0700

    Update surface.py

--- src/math/surface.py
+++ src/math/surface.py
@@ -1,40 +1,40 @@
 def triangle():
     typ = input("common, equilateral, right, isosceles >>> ")
     if typ == "common":
-        base = int(input("ฐาน(cm) >>> "))
-        high = int(input("สูง(cm) >>> "))
+        base = int(input("base(cm) >>> "))
+        high = int(input("height(cm) >>> "))
         print(0.5 * base * high)
     elif typ == "equilateral":
-        side = int(input("ด้าน(cm) >>> "))
+        side = int(input("side(cm) >>> "))
         print((3**0.5/4) * (side**2))
     elif typ == "right":
-        sidea = int(input("ด้านประกอบมุมฉากa(cm) >>> "))
-        sideb = int(input("ด้านประกอบมุมฉากb(cm) >>> "))
+        sidea = int(input("right angle 1(cm) >>> "))
+        sideb = int(input("right angle 2(cm) >>> "))
         anw = sidea * sideb
         print(0.5 * anw)
     elif typ == "isosceles":
-        base = int(input("ฐาน(cm) >>> "))
-        sidea = int(input("ด้านประกอบฐานa(cm) >>> "))
-        sideb = int(input("ด้านประกอบฐานb(cm) >>> "))
+        base = int(input("base(cm) >>> "))
+        sidea = int(input("base side 1(cm) >>> "))
+        sideb = int(input("base side 2(cm) >>> "))
         anw = ((sidea * sideb)**2 - (base)**2)**(1/4)
         print("%.3f" %(base/4 * anw))
 
 def cube():
     typ = input("squre, rectangle, rhombus, parallelogram, kite, trapezoid >>> ")
     if typ == "squre" or typ == "rectangle" or typ == "rhombus" or typ == "parallelogram":
-        base = int(input("ฐาน(cm) >>> "))
-        high = int(input("สูง(cm) >>> "))
+        base = int(input("base(cm) >>> "))
+        high = int(input("height(cm) >>> "))
         print(base * high)
     elif typ == "kite":
-        line1 = int(input("เส้นทะแยงมุม1(cm) >>> "))
-        line2 = int(input("เส้นทะแยงมุม2(cm) >>> "))
+        line1 = int(input("diagonal1(cm) >>> "))
+        line2 = int(input("diagonal2(cm) >>> "))
         print(line1 * line2 * 0.5)
     elif typ == "trapezoid":
-        dif1 = int(input("ด้านคู่ขนาน1(cm) >>> "))
-        dif2 = int(input("ด้านคู่ขนาน2(cm) >>> "))
-        high = int(input("สูง(cm) >>> "))
+        dif1 = int(input("parallel side 1(cm) >>> "))
+        dif2 = int(input("parallel side 2(cm) >>> "))
+        high = int(input("height(cm) >>> "))
         print(0.5 * high * (dif1+dif2))
 
 def circle():
-    radius = int(input("รัศมี(cm) >>> "))
+    radius = int(input("radius(cm) >>> "))
     print("%.3f" %(22/7 * radius**2))
