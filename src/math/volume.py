1a2a34d6f7cb0a5d09fadee0e572ab972d1cc14b
it63070045 <73681882+it63070045@users.noreply.github.com>
2020-12-14 15:41:15 +0700

    Update volume.py

--- src/math/volume.py
+++ src/math/volume.py
@@ -1,25 +1,25 @@
 import math
 
 def cylinder():#ทรงกระบอก
-    radius = int(input("ใส่ค่ารัศมี(cm) >>> "))
+    radius = int(input("radius(cm) >>> "))
     space = math.pi * radius**2
-    high = int(input("ใส่ค่าความสูง(cm) >>> "))
+    high = int(input("height(cm) >>> "))
     print("%.3f" %(high * space))
 
 def cube():#เหลียม
-    side = int(input("ใส่ค่าของด้าน(cm) >>> "))
-    high = int(input("ใส่ค่าความสูง(cm) >>> "))
+    side = int(input("side(cm) >>> "))
+    high = int(input("height(cm) >>> "))
     print("%.3f" %(side**2 * high))
 
 def circle():#วงกลม
-    radius = int(input("ใส่ค่ารัศมี(cm) >>> "))
+    radius = int(input("radius(cm) >>> "))
     print("%.3f" %(4 * math.pi * radius**3 / 3))
 
 def pyramid():#ปิรามิด
     typ = input("triangle, cube >>> ")
-    lon = int(input("ใส่ค่าของด้าน(cm) >>> "))
-    highl = int(input("ใส่ค่าของด้าน(cm) >>> "))
-    high = int(input("ใส่ค่าความสูง(cm) >>> "))
+    lon = int(input("side1(cm) >>> "))
+    highl = int(input("side2(cm) >>> "))
+    high = int(input("height(cm) >>> "))
     volume = lon * highl
     if typ == "triangle":
         print("%.3f" %((volume * 0.5) * high/3))
@@ -27,23 +27,23 @@ def pyramid():#ปิรามิด
         print("%.3f" %(volume * high/3))
 
 def conical():#ทรงกรวย
-    radius = int(input("ใส่ค่ารัศมี(cm) >>> "))
-    high = int(input("ใส่ค่าความสูง(cm) >>> "))
+    radius = int(input("radius(cm) >>> "))
+    high = int(input("height(cm) >>> "))
     print("%.3f" %(math.pi * radius**2 * high / 3))
 
 def prism():#ปริซึม
     typ = input("triangle, cube >>> ")
     if typ == "triangle" or typ == "cube":
-        lon = int(input("ใส่ค่าของด้าน(cm) >>> "))
-        highl = int(input("ใส่ค่าของด้าน(cm) >>> "))
-        high = int(input("ใส่ค่าความสูง(cm) >>> "))
+        lon = int(input("side1(cm) >>> "))
+        highl = int(input("side2(cm) >>> "))
+        high = int(input("height(cm) >>> "))
         volume = lon * highl
         if typ == "triangle":
             print("%.3f" %((volume * 0.5) * high))
         elif typ == "cube":
             print(volume * high)
     elif typ == "circle":
-        radius = int(input("ใส่ค่ารัศมี(cm) >>> "))
-        high = int(input("ใส่ค่าความสูง(cm) >>> "))
+        radius = int(input("radius(cm) >>> "))
+        high = int(input("height(cm) >>> "))
         cir = radius * math.pi
         print("%.3f" %(cir * high))
